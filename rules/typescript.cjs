598533d7f094767192c4133323606bb34a52a187
Heunsig <heun3344@gmail.com>
2023-01-21 17:57:02 +0900

    typescript 룰 추가

--- rules/typescript.cjs
+++ rules/typescript.cjs
@@ -187,5 +187,114 @@ module.exports = {
      * @reason 코드에 영향을 주진 않아서 너무 Strict 할 필요 없을 듯 함
      */
     '@typescript-eslint/no-confusing-void-expression': 'off',
+    /**
+     * 중복 class 멤버 정의 금지
+     * @reason 컴파일 단계에서 에러 처리됨
+     */
+    'no-dupe-class-members': 'off',
+    '@typescript-eslint/no-dupe-class-members': 'off',
+    /**
+     * enum에서 중복 값 사용 금지
+     */
+    '@typescript-eslint/no-duplicate-enum-values': 'error',
+    /**
+     * delete 시 동적 key 값 사용 금지
+     * const name = 'david' [X]
+     * delete member[name]
+     * ----------------------
+     * delete member.david [O]
+     */
+    '@typescript-eslint/no-dynamic-delete': 'off',
+    /**
+     * 빈 함수 허용 안 함
+     * @reason 빈 함수를 기본값으로 설정해야되는 경우도 있음
+     * TODO: 논의 필요
+     */
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    /**
+     * 빈 Interface 사용 안 함
+     */
+    '@typescript-eslint/no-empty-interface': 'error',
+    /**
+     * any 사용 금지
+     * @reason Typescript 적응 단계에서는 any 사용이 필요
+     */
+    '@typescript-eslint/no-explicit-any': 'off',
+    /**
+     * 불필요한 non-null assertion 사용 안 함
+     * const bar = foo!!!.bar [X], const bar = foo!.bar [O]
+     */
+    '@typescript-eslint/no-extra-non-null-assertion': 'off',
+    /**
+     * 예외처리 하지 않은 Promise 사용 안 함
+     */
+    '@typescript-eslint/no-floating-promises': 'off',
+    /**
+     * array에서 for in 루프 사용 안 함
+     */
+    '@typescript-eslint/no-for-in-array': 'off',
+    /**
+     * eval 사용 금지
+     * TODO: 논의 필요, error 처리하는게 좋을 듯함
+     */
+    'no-implied-eval': 'off',
+    '@typescript-eslint/no-implied-eval': 'off',
+    /**
+     * 변수 선언시 값을 바로 할당하는 경우 type을 명식적으로 작성 안 함
+     * @reason 명시적으로 type 설정을 안하는게 코드 단순화 할 수 있음
+     * TODO: 논의 필요
+     */
+    '@typescript-eslint/no-inferrable-types': 'error',
+    /**
+     * this가 undefined인 곳에서 this 사용 안 함
+     */
+    'no-invalid-this': 'off',
+    '@typescript-eslint/no-invalid-this': 'error',
+    /**
+     * 무의미한 void 유형 사용 안 함
+     * @reason void는 함수 반환값 타입으로만 사용하면 됨
+     */
+    '@typescript-eslint/no-invalid-void-type': 'error',
+    /**
+     * 반복문 내 함수에서 반목문 조건에 해당하는 변수 사용 금지
+     * @reason let 사용하면 해결됨
+     */
+    'no-loop-func': 'off',
+    '@typescript-eslint/no-loop-func': 'off',
+    /**
+     * Javascript가 인정하는 범위를 넘긴 숫자 사용 금지
+     * const num = 5123000000000000000000000000001 [X]
+     */
+    'no-loss-of-precision': 'off',
+    '@typescript-eslint/no-loss-of-precision': 'error',
+    /**
+     * magic numbers 사용 안 함
+     */
+    'no-magic-numbers': 'off',
+    '@typescript-eslint/no-magic-numbers': 'off',
+    /**
+     * void가 null을 반환하는 것 금지
+     */
+    '@typescript-eslint/no-meaningless-void-operator': 'off',
+    /**
+     * Interface에서 constructor를 정의하거나 Class에서 new를 멤버로 정의 안 함
+     * Alloy Team 에서는 off 처리
+     */
+    '@typescript-eslint/no-misused-new': 'error',
+    /**
+     * 잘못된 Promise 사용 금지
+     * Alloy Team 에서는 off 처리
+     */
+    '@typescript-eslint/no-misused-promises': 'error',
+    /**
+     * Typescript의 namespace 기능 사용 안 함
+     * @reason es6의 모듈방식으로 가져오는게 일반적임
+     */
+    '@typescript-eslint/no-namespace': 'error',
+    /**
+     * null이 아닌 
+     */
+    '@typescript-eslint/no-non-null-asserted-nullish-coalescing': 'error',
   }
 };
