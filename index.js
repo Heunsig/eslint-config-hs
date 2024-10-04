23127a5d8cb672892d362a9a4ec8ebe027cd998e
Heunsig <heun3344@gmail.com>
2024-10-04 22:39:00 +0900

    refactor: update eslint configuration structure

--- index.js
+++ index.js
@@ -4,32 +4,63 @@ import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
 import stylistic from "@stylistic/eslint-plugin";
 
-export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts,vue}"],
-  },
-  {
-    languageOptions: {
-      globals: globals.browser,
-    },
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/recommended"],
-  {
-    files: ["**/*.vue"],
-    languageOptions: { parserOptions: { parser: tseslint.parser } },
-  },
-  stylistic.configs.customize({
-    quotes: "double",
-    commaDangle: "only-multiline",
-    semi: true,
-    blockSpacing: true,
-    indent: 2,
-    arrowParens: false,
-    braceStyle: "stroustrup",
-    quoteProps: "consistent-as-needed",
-    jsx: true,
-    flat: true,
-  }),
-];
+export default {
+  configs: {
+    javascript: [
+      {
+        files: ["**/*.{js,mjs,cjs,vue}"],
+      },
+      {
+        languageOptions: {
+          globals: globals.browser,
+        },
+      },
+      pluginJs.configs.recommended,
+      ...pluginVue.configs["flat/recommended"],
+      {
+        files: ["**/*.vue"],
+      },
+      stylistic.configs.customize({
+        quotes: "double",
+        commaDangle: "only-multiline",
+        semi: true,
+        blockSpacing: true,
+        indent: 2,
+        arrowParens: false,
+        braceStyle: "stroustrup",
+        quoteProps: "consistent-as-needed",
+        jsx: true,
+        flat: true,
+      }),
+    ],
+    typescript: [
+      {
+        files: ["**/*.{js,mjs,cjs,ts,vue}"],
+      },
+      {
+        languageOptions: {
+          globals: globals.browser,
+        },
+      },
+      pluginJs.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...pluginVue.configs["flat/recommended"],
+      {
+        files: ["**/*.vue"],
+        languageOptions: { parserOptions: { parser: tseslint.parser } },
+      },
+      stylistic.configs.customize({
+        quotes: "double",
+        commaDangle: "only-multiline",
+        semi: true,
+        blockSpacing: true,
+        indent: 2,
+        arrowParens: false,
+        braceStyle: "stroustrup",
+        quoteProps: "consistent-as-needed",
+        jsx: true,
+        flat: true,
+      }),
+    ]
+  }
+}
