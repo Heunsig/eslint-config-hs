b12297f753c5ef03f3481daa57681fd5ee7383b4
Heunsig <heun3344@gmail.com>
2024-10-04 21:50:38 +0900

    feat: add new version eslint config

---
+++ index.js
@@ -0,0 +1,35 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import stylistic from "@stylistic/eslint-plugin";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,vue}"],
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/recommended"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+  stylistic.configs.customize({
+    quotes: "double",
+    commaDangle: "only-multiline",
+    semi: true,
+    blockSpacing: true,
+    indent: 2,
+    arrowParens: false,
+    braceStyle: "stroustrup",
+    quoteProps: "consistent-as-needed",
+    jsx: true,
+    flat: true,
+  }),
+];
