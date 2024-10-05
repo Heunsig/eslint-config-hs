d760ad7933b3ae9037bc899a59e665fadd6577ed
Heunsig <heun3344@gmail.com>
2024-10-05 13:18:00 +0900

    refactor: update package version to 2.0.0-rc.10 and fix module type in package.json

--- index.cjs
+++ index.cjs
@@ -3,27 +3,29 @@ const pluginJs = require("@eslint/js");
 const stylistic = require("@stylistic/eslint-plugin");
 
 module.exports = {
-  configs: [
-    {
-      files: ["**/*.{js,mjs,cjs}"],
-    },
-    {
-      languageOptions: {
-        globals: globals.browser,
+  configs: {
+    javascript: [
+      {
+        files: ["**/*.{js,mjs,cjs}"],
       },
-    },
-    pluginJs.configs.recommended,
-    stylistic.configs.customize({
-      quotes: "double",
-      commaDangle: "only-multiline",
-      semi: true,
-      blockSpacing: true,
-      indent: 2,
-      arrowParens: false,
-      braceStyle: "stroustrup",
-      quoteProps: "consistent-as-needed",
-      jsx: true,
-      flat: true,
-    }),
-  ],
+      {
+        languageOptions: {
+          globals: globals.browser,
+        },
+      },
+      pluginJs.configs.recommended,
+      stylistic.configs.customize({
+        quotes: "double",
+        commaDangle: "only-multiline",
+        semi: true,
+        blockSpacing: true,
+        indent: 2,
+        arrowParens: false,
+        braceStyle: "stroustrup",
+        quoteProps: "consistent-as-needed",
+        jsx: true,
+        flat: true,
+      }),
+    ],
+  }
 };
