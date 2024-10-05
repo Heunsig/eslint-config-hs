8e311f0aedc0c89cdbf3f841b503a4762db8c688
Heunsig <heun3344@gmail.com>
2024-10-05 13:57:40 +0900

    refactor: Update package version to 2.0.0-rc.14 and update language options in index.cjs and index.js

--- index.cjs
+++ index.cjs
@@ -4,7 +4,38 @@ const stylistic = require("@stylistic/eslint-plugin");
 
 module.exports = {
   configs: {
-    javascript: [
+    'js': [
+      {
+        files: ["**/*.{js,mjs,cjs}"],
+      },
+      {
+        languageOptions: {
+          globals: globals.browser,
+        }
+      },
+      pluginJs.configs.recommended,
+      stylistic.configs.customize({
+        quotes: "double",
+        commaDangle: "only-multiline",
+        semi: true,
+        blockSpacing: true,
+        indent: 2,
+        arrowParens: false,
+        braceStyle: "stroustrup",
+        quoteProps: "consistent-as-needed",
+        jsx: true,
+        flat: true,
+      }),
+    ],
+    'node-js': [
+      {
+        files: ["**/*.{js,mjs,cjs}"],
+      },
+      {
+        languageOptions: {
+          globals: globals.node,
+        }
+      },
       pluginJs.configs.recommended,
       stylistic.configs.customize({
         quotes: "double",
--- index.js
+++ index.js
@@ -6,7 +6,111 @@ import stylistic from "@stylistic/eslint-plugin";
 
 export default {
   configs: {
-    javascript: [
+    'js': [
+      {
+        files: ["**/*.{js,mjs,cjs}"],
+      },
+      {
+        languageOptions: {
+          globals: globals.browser,
+        }
+      },
+      pluginJs.configs.recommended,
+      ...pluginVue.configs["flat/recommended"],
+      stylistic.configs.customize({
+        quotes: "double",
+        commaDangle: "only-multiline",
+        semi: true,
+        blockSpacing: true,
+        indent: 2,
+        arrowParens: false,
+        braceStyle: "stroustrup",
+        quoteProps: "consistent-as-needed",
+        jsx: true,
+        flat: true,
+      }),
+    ],
+    'ts': [
+      {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+      },
+      {
+        languageOptions: {
+          globals: globals.browser,
+        }
+      },
+      pluginJs.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...pluginVue.configs["flat/recommended"],
+      stylistic.configs.customize({
+        quotes: "double",
+        commaDangle: "only-multiline",
+        semi: true,
+        blockSpacing: true,
+        indent: 2,
+        arrowParens: false,
+        braceStyle: "stroustrup",
+        quoteProps: "consistent-as-needed",
+        jsx: true,
+        flat: true,
+      }),
+    ],
+    'node-js': [
+      {
+        files: ["**/*.{js,mjs,cjs}"],
+      },
+      {
+        languageOptions: {
+          globals: globals.node,
+        }
+      },
+      pluginJs.configs.recommended,
+      stylistic.configs.customize({
+        quotes: "double",
+        commaDangle: "only-multiline",
+        semi: true,
+        blockSpacing: true,
+        indent: 2,
+        arrowParens: false,
+        braceStyle: "stroustrup",
+        quoteProps: "consistent-as-needed",
+        jsx: true,
+        flat: true,
+      }),
+    ],
+    'node-ts': [
+      {
+        files: ["**/*.{js,mjs,cjs}"],
+      },
+      {
+        languageOptions: {
+          globals: globals.node,
+        }
+      },
+      pluginJs.configs.recommended,
+      ...tseslint.configs.recommended,
+      stylistic.configs.customize({
+        quotes: "double",
+        commaDangle: "only-multiline",
+        semi: true,
+        blockSpacing: true,
+        indent: 2,
+        arrowParens: false,
+        braceStyle: "stroustrup",
+        quoteProps: "consistent-as-needed",
+        jsx: true,
+        flat: true,
+      }),
+    ],
+    'vue-js': [
+      {
+        files: ["**/*.{js,mjs,cjs,vue}"],
+      },
+      {
+        languageOptions: {
+          globals: globals.browser,
+        }
+      },
       pluginJs.configs.recommended,
       ...pluginVue.configs["flat/recommended"],
       {
@@ -25,7 +129,15 @@ export default {
         flat: true,
       }),
     ],
-    typescript: [
+    'vue-ts': [
+      {
+        files: ["**/*.{js,mjs,cjs,ts,vue}"],
+      },
+      {
+        languageOptions: {
+          globals: globals.browser,
+        }
+      },
       pluginJs.configs.recommended,
       ...tseslint.configs.recommended,
       ...pluginVue.configs["flat/recommended"],
