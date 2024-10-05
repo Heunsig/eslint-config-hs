1daf15813841d55bf5a7ea4bda2e8501eb9d2d2d
Heunsig <heun3344@gmail.com>
2024-10-06 00:03:56 +0900

    Refactor file imports and add ignoreOption.js

---
+++ nuxt.js
@@ -0,0 +1,29 @@
+import pluginJs from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+import tseslint from "typescript-eslint";
+import tsStylistic from "./stylistic-ts.js";
+import ignoreOption from "./ignoreOption.js";
+
+export default [
+  ignoreOption,
+  {
+    ignores: [
+      '.output/*',
+      '.data/*',
+      '.nuxt/*',
+      '.nitro/*',
+      '.cache/*'
+    ]
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,vue}"],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/recommended"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+  ...tsStylistic
+]
