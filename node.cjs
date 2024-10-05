2e39298205a50797772ef85724e19916528ee698
Heunsig <heun3344@gmail.com>
2024-10-05 20:35:43 +0900

    Refactor file imports and update package version to 2.0.0-rc.26

--- node.cjs
+++ node.cjs
@@ -1,3 +1,7 @@
+const globals = require("globals");
+const pluginJs = require("@eslint/js");
+const stylistic = require("@stylistic/eslint-plugin");
+
 module.exports = [
   {
     files: ["**/*.{js,mjs,cjs}"],
